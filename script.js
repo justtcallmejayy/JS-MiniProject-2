1730e295ba1f00f925cef7c3f394d8ac655f12db
Choksi <000887533@mohawkcollege.ca>
2024-11-05 18:32:35 -0500

    Added logic of levelup and check user answer.

--- script.js
+++ script.js
@@ -37,19 +37,44 @@ function userFlash(btn) {
     },
         250);
 }
+// level up fnc is very imp here
+//also i am add userSeq[] empty so that it can start over again for the user everytime the new color flashes!
+//This is 
 
 function levelUp() {
+    userSeq=[]; //This will reset the game sequence
     level++;
     h2.innerText = `Your level is : ${level}`
 
     let randIndex = Math.floor(Math.random() * buttons.length);
     let randColor = buttons[randIndex];
     let randButton = document.querySelector(`.${randColor}`)
-    console.log(randButton);
-    console.log(randColor);
-    console.log(randIndex);
+    // console.log(randButton);
+    // console.log(randColor);
+    // console.log(randIndex);
+
+    gameSeq.push(randColor);
+    console.log(gameSeq);
 
     gameFlash(randButton);
+}
+
+function checkUserColor(indexLevel) {
+    // console.log(`Current Level : ${level}`);
+    h2.innerText = `Current Level : ${level}`;
+
+    if (userSeq[indexLevel] === gameSeq[indexLevel]) {
+        // console.log("Similair value");
+        if (userSeq.length == gameSeq.length) {
+            setTimeout (levelUp,1000);
+        }
+        // h2.innerText = `Keep going you `
+    }
+    else {
+        // console.log(`Game is over, Please try again by resetting the level`);
+        h2.innerText = `Game is over, Please try again by resetting the level`
+
+    }
 }
 
 //code for button pressed!
@@ -57,6 +82,12 @@ function levelUp() {
 function btnPressed() {
     let btn = this;
     userFlash(btn);
+
+    let userColor = btn.getAttribute("id");
+    userSeq.push(userColor);
+
+    //logging the level of the user, the current level value will be the same as the current sequence and user score
+    checkUserColor(userSeq.length-1);
 }
 
 let allBtns = document.querySelectorAll(".btn");
